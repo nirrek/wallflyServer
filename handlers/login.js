89346ab043efcd1bf0f8c9d6f9b5b64d3aaa9b06
kerrin <kerrinenglish@gmail.com>
2015-10-04 20:22:40 +1000

    Add validation for Login requests

--- handlers/login.js
+++ handlers/login.js
@@ -3,18 +3,9 @@ var validate = require('../validation.js').validate;
 function loginHandler(request, reply) {
   var username = request.payload.username;
   var password = request.payload.password;
-  var message = '';
 
-  // Handle missing credentials
-  if (!username || !password) {
-    message = 'Missing username or password';
-    return reply(message).code(401);
-  }
-
-  // Fetch user the user model from the database
-  // var account = users[username];
   validate(request, username, password, function(err, isValid, user) {
-    if (!isValid) return reply('Invalid username or password');
+    if (!isValid) return reply('Invalid username or password.').code(422);
 
     // Add the users id only to the session. The session data is passed around
     // as base64 ecnoded data in the cookie. Importantly, if the encoding is
