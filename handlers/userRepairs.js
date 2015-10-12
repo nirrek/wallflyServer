20dfb6890c9521df50e18c19482533f66dae673a
Aulia Agung Maulana <auliaagungmaulana@Maulanas-MacBook-Pro.local>
2015-10-13 09:17:06 +1000

    Support lodge a new repair request form for adding a new image

--- handlers/userRepairs.js
+++ handlers/userRepairs.js
@@ -15,7 +15,7 @@ function userRepairs(request, reply) {
 function getHandler(request, reply, userId) {
   pool.getConnection(function(err, connection) {
     connection.query({
-      sql: 'SELECT id, date, request, photo, status ' +
+      sql: 'SELECT id, date, request, status ' +
            'FROM repair_requests WHERE tenantId = ?',
       values: [userId],
     }, function(err, results) {
@@ -30,6 +30,8 @@ function postHandler(request, reply, userId) {
   var payload = request.payload;
 
   pool.getConnection(function(err, connection) {
+    var image = request.payload.image;
+    var requesttId = request.payload.requestId;
     connection.query({
       sql: 'SELECT id FROM properties WHERE tenantId = ?',
       values: [userId]
@@ -42,11 +44,10 @@ function postHandler(request, reply, userId) {
 
       connection.query({
         sql: 'INSERT INTO repair_requests ' +
-             '(request, photo, tenantId, propertyId) ' +
-             'VALUES (?, ?, ?, ?)',
+             '(request, tenantId, propertyId) ' +
+             'VALUES (?, ?, ?)',
         values:[
           payload.description,
-          payload.image,
           userId,
           propertyId
         ],
@@ -56,8 +57,26 @@ function postHandler(request, reply, userId) {
           reply(err).code(500);
           return;
         }
+        var requestId = results[1].id;
+        console.log(requestId);
+
+          connection.query({
+          sql: 'INSERT INTO repair_request_images ' +
+               '(photo, requestId) ' +
+               'VALUES (?, ?)',
+          values:[
+            image,
+            requestId,
+          ],
+        }, function(err, results) {
+          connection.release();
+          if (err) {
+            reply(err).code(500);
+            return;
+          }
 
         reply('Repair Request added successfully');
+        });
       });
     });
   });
